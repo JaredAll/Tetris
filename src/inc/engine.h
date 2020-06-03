f57ad964f016406887cf5a1a0e81389ea1817bef
Jared Allen <jla4782@truman.edu>
2020-06-03 18:17:54 -0500

    initialize engine window

---
+++ src/inc/engine.h
@@ -0,0 +1,14 @@
+#include <SDL.h>
+
+#ifndef JAREDALL_TETRIS_ENGINE
+#define JAREDALL_TETRIS_ENGINE
+
+class Engine
+{
+public:
+
+  void initialize();
+  
+};
+
+#endif
---
+++ src/main/engine/engine.cpp
@@ -0,0 +1,23 @@
+#include "engine.h"
+#include <iostream>
+
+void Engine::initialize()
+{
+  if (SDL_Init(SDL_INIT_VIDEO) != 0)
+  {
+    std::cout << "SDL_Init failed" << std::endl;
+  }
+
+  int window_width = 1000;
+  int window_height = 500;
+  SDL_Window *win = SDL_CreateWindow("Labyrinth sketch 1",
+                                     SDL_WINDOWPOS_CENTERED,
+                                     SDL_WINDOWPOS_CENTERED,
+                                     window_width, window_height,
+                                     SDL_WINDOW_SHOWN);
+  if (win == nullptr)
+  {
+    std::cout << "Create Window failed" << std::endl;
+    SDL_Quit();
+  }
+}
