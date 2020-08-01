86b406654a02f382f0479a12761c41b3f5090f43
Jared Allen <jla4782@truman.edu>
2020-08-01 12:39:59 -0500

    factored out render component mock

--- test/main/tetris_piece_test.cpp
+++ test/main/tetris_piece_test.cpp
@@ -4,11 +4,13 @@
 #include "bar_piece.h"
 #include "render_component.h"
 #include "SDL_render.h"
+#include "render_component_mock.h"
 #include <memory>
 
 using std::unique_ptr;
 using std::make_unique;
 using std::move;
+using trompeloeil::_;
 
 TEST_CASE( "test constructor" )
 {
@@ -60,23 +62,6 @@ TEST_CASE( "determine block locations" )
   }
 }
 
-class RenderComponentMock : public RenderComponent
-{
-public:
-  MAKE_CONST_MOCK0( getTexture, std::shared_ptr<SDL_Texture>(), override );
-  MAKE_CONST_MOCK0( getDestination, std::shared_ptr<SDL_Rect>(), override );
-  MAKE_CONST_MOCK0( getClip, std::shared_ptr<SDL_Rect>(), override );
-  MAKE_MOCK1( set_clip, void( std::shared_ptr<SDL_Rect> ), override );
-  MAKE_MOCK1( set_destination, void( std::shared_ptr<SDL_Rect> ), override );
-  MAKE_MOCK0( get_x, int(), override );
-  MAKE_MOCK0( get_y, int(), override );
-  MAKE_MOCK0( get_h, int(), override );
-  MAKE_MOCK0( get_w, int(), override );
-  MAKE_MOCK1( set_x, void( int ), override );
-  MAKE_MOCK1( set_y, void( int ), override );
-  MAKE_MOCK0( calculate_destination, void(), override );
-};
-
 TEST_CASE( "determine block locations rotated" )
 {
   unique_ptr<TetrisPiece> piece = make_unique<BarPiece>();
@@ -84,23 +69,23 @@ TEST_CASE( "determine block locations rotated" )
   int num_bar_blocks = 4;
 
   unique_ptr<RenderComponentMock> mock0 = make_unique<RenderComponentMock>();
-  REQUIRE_CALL( *mock0, set_x( 5 ) );
-  REQUIRE_CALL( *mock0, set_y( 0 ) );
+  REQUIRE_CALL( *mock0, set_x( _ ) );
+  REQUIRE_CALL( *mock0, set_y( _ ) );
   piece -> add_render_component( move( mock0 ) );
 
   unique_ptr<RenderComponentMock> mock1 = make_unique<RenderComponentMock>();
-  REQUIRE_CALL( *mock1, set_x( 5 ) );
-  REQUIRE_CALL( *mock1, set_y( 1 ) );
+  REQUIRE_CALL( *mock1, set_x( _ ) );
+  REQUIRE_CALL( *mock1, set_y( _ ) );
   piece -> add_render_component( move( mock1 ) );
 
   unique_ptr<RenderComponentMock> mock2 = make_unique<RenderComponentMock>();
-  REQUIRE_CALL( *mock2, set_x( 5 ) );
-  REQUIRE_CALL( *mock2, set_y( 2 ) );
+  REQUIRE_CALL( *mock2, set_x( _ ) );
+  REQUIRE_CALL( *mock2, set_y( _ ) );
   piece -> add_render_component( move( mock2 ) );
 
   unique_ptr<RenderComponentMock> mock3 = make_unique<RenderComponentMock>();
-  REQUIRE_CALL( *mock3, set_x( 5 ) );
-  REQUIRE_CALL( *mock3, set_y( 3 ) );
+  REQUIRE_CALL( *mock3, set_x( _ ) );
+  REQUIRE_CALL( *mock3, set_y( _ ) );
   piece -> add_render_component( move( mock3 ) );
 
   SECTION( "test get_bottom_row rotated" )
