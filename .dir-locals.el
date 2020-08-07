6419c6f306045f2eec701f6f975e42c216f165c9
Jared Allen <jla4782@truman.edu>
2020-08-06 21:22:39 -0500

    added concrete piece tests

--- .dir-locals.el
+++ .dir-locals.el
@@ -2,5 +2,5 @@
       (
        (relative-compile-command . "cd ~/Games/Tetris && bear make -C ~/Games/Tetris/build")
        (run-command . "~/Games/Tetris/build/Tetris")
-       (test-command . "~/Games/Tetris/build/tests")
+       (test-command . "~/Games/Tetris/build/tests --use-colour no")
         )))
