0b65e30f3bd04b9e228487e7e333700a3ecf2bc3
Jared Allen <jla4782@truman.edu>
2020-09-14 12:23:16 -0500

    used appropriate citations for easy sdl

--- src/main/engine/easy_sdl.cpp
+++ src/main/engine/easy_sdl.cpp
@@ -1,24 +1,28 @@
+/**
+   The functions in this file are due to Lazy Foo' Productions: 
+   https://lazyfoo.net/tutorials/SDL/index.php
+*/
+
 #include "easy_sdl.h"
-void logSDLError( std::ostream &os, const std::string &msg )
-{
-  os << msg << " error: " << SDL_GetError() << std::endl;
-}
+#include "SDL_error.h"
 
 SDL_Texture* loadTexture(const std::string &file, SDL_Renderer *ren)
 {
   SDL_Texture *texture = IMG_LoadTexture( ren, file.c_str() );
   if ( texture == nullptr )
-    {
-    logSDLError( std::cout, "LoadTexture" );
+  {
+    std::cout << "LoadTexture error: " << SDL_GetError() << std::endl;
   }
   return texture;
 }
 
-void renderTexture(SDL_Texture *tex, SDL_Renderer *ren,
-                   int x, int y, SDL_Rect *clip,
+void renderTexture(SDL_Texture *tex, 
+                   SDL_Renderer *ren,
+                   int x, 
+                   int y, 
+                   SDL_Rect *clip,
                    SDL_Rect* dst )
 {
-  //Setup the destination rectangle to be at the position we want
   dst -> x = x;
   dst -> y = y;
   if( clip != nullptr )
@@ -28,17 +32,18 @@ void renderTexture(SDL_Texture *tex, SDL_Renderer *ren,
   }
   else
   {
-    //Query the texture to get its width and height to use
     SDL_QueryTexture(tex, NULL, NULL, &( dst -> w ), &( dst -> h ) );
     SDL_RenderCopy(ren, tex, NULL, dst);
   }
   renderTexture( tex, ren, dst, clip );
 }
 
-void renderTextureFlip(SDL_Texture *tex, SDL_Renderer *ren,
-                   int x, int y, SDL_Rect *clip )
+void renderTextureFlip(SDL_Texture *tex, 
+                       SDL_Renderer *ren,
+                       int x, 
+                       int y, 
+                       SDL_Rect *clip )
 {
-  //Setup the destination rectangle to be at the position we want
   SDL_Rect *dst = new SDL_Rect();
   dst -> x = x;
   dst -> y = y;
@@ -49,7 +54,6 @@ void renderTextureFlip(SDL_Texture *tex, SDL_Renderer *ren,
   }
   else
   {
-    //Query the texture to get its width and height to use
     SDL_QueryTexture(tex, NULL, NULL, &( dst -> w ), &( dst -> h ) );
     SDL_RenderCopyEx(ren, tex, NULL, dst, 0, 0,
                      SDL_FLIP_HORIZONTAL);
@@ -57,16 +61,20 @@ void renderTextureFlip(SDL_Texture *tex, SDL_Renderer *ren,
   renderTextureFlip( tex, ren, dst, clip );
 }
 
-void renderTextureFlip( SDL_Texture *tex, SDL_Renderer *ren,
-                    SDL_Rect *dst, SDL_Rect *clip )
+void renderTextureFlip( SDL_Texture *tex, 
+                        SDL_Renderer *ren,
+                        SDL_Rect *dst, 
+                        SDL_Rect *clip )
 {
   SDL_RenderCopyEx( ren, tex, clip, dst, 0, 0,
                     SDL_FLIP_HORIZONTAL );
 }
 
 
-void renderTexture( SDL_Texture *tex, SDL_Renderer *ren,
-                    SDL_Rect *dst, SDL_Rect *clip )
+void renderTexture( SDL_Texture *tex, 
+                    SDL_Renderer *ren,
+                    SDL_Rect *dst,
+                    SDL_Rect *clip )
 {
   SDL_RenderCopy( ren, tex, clip, dst );
 }
